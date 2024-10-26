8c9bfaf1ffe790170f5dad184f142494c97c52ff
LeoKids <68135774+LeoKids@users.noreply.github.com>
2024-10-26 11:15:45 -0300

    Update nsNSSComponent.cpp
    Won't work without changing this

--- security/manager/ssl/src/nsNSSComponent.cpp
+++ security/manager/ssl/src/nsNSSComponent.cpp
@@ -953,9 +953,9 @@ static CipherPref CipherPrefs[] = {
  {"security.ssl3.ecdhe_ecdsa_aes_128_gcm_sha256", TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256}, // 128-bit AES-GCM encryption with ECDHE-ECDSA
  {"security.ssl3.ecdhe_rsa_aes_128_gcm_sha256", TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256}, // 128-bit AES-GCM encryption with ECDHE-RSA
  {"security.ssl3.ecdhe_rsa_chacha20_poly1305_sha256", TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256}, // 256-bit ChaCha20-Poly1305 encryption with ECDHE-RSA
- {"security.tls13.chacha20_poly1305_sha256", TLS_CHACHA20_POLY1305_SHA256}, // TLS 1.3 256-bit ChaCha20-Poly1305 encryption
- {"security.tls13.aes_128_gcm_sha256", TLS_AES_128_GCM_SHA256}, // TLS 1.3 128-bit AES-GCM encryption
- {"security.tls13.aes_256_gcm_sha384", TLS_AES_256_GCM_SHA384}, // TLS 1.3 256-bit AES-GCM encryption
+ {"security.ssl3.chacha20_poly1305_sha256", TLS_CHACHA20_POLY1305_SHA256}, // TLS 1.3 256-bit ChaCha20-Poly1305 encryption
+ {"security.ssl3.aes_128_gcm_sha256", TLS_AES_128_GCM_SHA256}, // TLS 1.3 128-bit AES-GCM encryption
+ {"security.ssl3.aes_256_gcm_sha384", TLS_AES_256_GCM_SHA384}, // TLS 1.3 256-bit AES-GCM encryption
  {NULL, 0} /* end marker */
 };
 
